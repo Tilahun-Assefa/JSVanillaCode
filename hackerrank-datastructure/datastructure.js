172e9aedf124978b0e910d5324c37aa451e4f6b9
Tilahun Demissie <til.assf98@gmail.com>
2023-12-17 17:18:00 -0600

    adding eslint and complete deleteNode

--- hackerrank-datastructure/datastructure.js
+++ hackerrank-datastructure/datastructure.js
@@ -1,3 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-jsdoc */
+/* eslint-disable linebreak-style */
 /*
  * Complete the 'hourglassSum' function below.
  *
@@ -6,16 +11,16 @@
  */
 
 function hourglassSum(a) {
-    // Write your code here
-    let maxSumHourGlass = Number.NEGATIVE_INFINITY;
-    for (let i = 0; i < a.length - 2; i++) {
-        let sum = 0;
-        for (let j = 0; j < a.length - 2; j++) {
-            sum = a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 1] + a[i + 2][j] + a[i + 2][j + 1] + a[i + 2][j + 2];
-            maxSumHourGlass = sum > maxSumHourGlass ? sum : maxSumHourGlass;
-        }
+  // Write your code here
+  let maxSumHourGlass = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < a.length - 2; i++) {
+    let sum = 0;
+    for (let j = 0; j < a.length - 2; j++) {
+      sum = a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 1] + a[i + 2][j] + a[i + 2][j + 1] + a[i + 2][j + 2];
+      maxSumHourGlass = sum > maxSumHourGlass ? sum : maxSumHourGlass;
     }
-    return maxSumHourGlass;
+  }
+  return maxSumHourGlass;
 }
 
 // console.log(hourglassSum([[-9, -9, -9, 1, 1, 1], [0, -9, 0, 4, 3, 2], [-9, -9, -9, 1, 2, 3], [0, 0, 8, 6, 6, 0], [0, 0, 0, -2, 0, 0], [0, 0, 1, 2, 4, 0]]) === 28);
@@ -31,25 +36,25 @@ function hourglassSum(a) {
  */
 
 function dynamicArray(n, queries) {
-    // Write your code here
-    let lastAnswer = 0;
-    const answers = [];
-    let arr = [];
-    for (let i = 0; i < n; i++) {
-        const el = [];
-        arr.push(el);
+  // Write your code here
+  let lastAnswer = 0;
+  const answers = [];
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    const el = [];
+    arr.push(el);
+  }
+  for (let i = 0; i < queries.length; i++) {
+    if (queries[i][0] === 1) {
+      const idx = (queries[i][1] ^ lastAnswer) % n;
+      arr[idx].push(queries[i][2]);
+    } else if (queries[i][0] === 2) {
+      const idx = (queries[i][1] ^ lastAnswer) % n;
+      lastAnswer = arr[idx][queries[i][2] % arr[idx].length];
+      answers.push(lastAnswer);
     }
-    for (let i = 0; i < queries.length; i++) {
-        if (queries[i][0] === 1) {
-            let idx = (queries[i][1] ^ lastAnswer) % n;
-            arr[idx].push(queries[i][2]);
-        } else if (queries[i][0] === 2) {
-            let idx = (queries[i][1] ^ lastAnswer) % n;
-            lastAnswer = arr[idx][queries[i][2] % arr[idx].length];
-            answers.push(lastAnswer);
-        }
-    }
-    return answers;
+  }
+  return answers;
 }
 
 // console.log(dynamicArray(2, [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]]));  //[7,3]
@@ -64,12 +69,12 @@ function dynamicArray(n, queries) {
  */
 
 function rotateLeft(d, arr) {
-    // Write your code here
-    for (let i = 0; i < d; i++) {
-        let el = arr.shift();
-        arr.push(el);
-    }
-    return arr;
+  // Write your code here
+  for (let i = 0; i < d; i++) {
+    const el = arr.shift();
+    arr.push(el);
+  }
+  return arr;
 }
 
 // console.log(rotateLeft(2, [1, 2, 3, 4, 5]));    //[3,4,5,1,2]
@@ -85,18 +90,18 @@ function rotateLeft(d, arr) {
  */
 
 function matchingStrings(stringList, queries) {
-    // Write your code here
-    let result = [];
-    for (let i = 0; i < queries.length; i++) {
-        let cnt = 0;
-        for (let j = 0; j < stringList.length; j++) {
-            if (queries[i] === stringList[j]) {
-                cnt++;
-            }
-        }
-        result.push(cnt);
+  // Write your code here
+  const result = [];
+  for (let i = 0; i < queries.length; i++) {
+    let cnt = 0;
+    for (let j = 0; j < stringList.length; j++) {
+      if (queries[i] === stringList[j]) {
+        cnt++;
+      }
     }
-    return result;
+    result.push(cnt);
+  }
+  return result;
 }
 
 // console.log(matchingStrings(["aba", "baba", "aba", "xzxb"], ["aba", "xzxb", "ab"]));    //[2,1,0]
@@ -114,25 +119,25 @@ function matchingStrings(stringList, queries) {
  */
 
 function arrayManipulation(n, queries) {
-    // Write your code here
-    let maxValue = Number.NEGATIVE_INFINITY;
-    const arr = Array(n).fill(0);
-    let cur = 0;
-    for (let i = 0; i < queries.length; i++) {
-        const query = queries[i];
-        let a = query[0];
-        let b = query[1];
-        let k = query[2];
-        arr[a - 1] += k;
-        if (b < n) {
-            arr[b] -= k;
-        }
+  // Write your code here
+  let maxValue = Number.NEGATIVE_INFINITY;
+  const arr = Array(n).fill(0);
+  let cur = 0;
+  for (let i = 0; i < queries.length; i++) {
+    const query = queries[i];
+    const a = query[0];
+    const b = query[1];
+    const k = query[2];
+    arr[a - 1] += k;
+    if (b < n) {
+      arr[b] -= k;
     }
-    for (let j = 0; j < arr.length; j++) {
-        cur += arr[j];
-        maxValue = Math.max(cur, maxValue);
-    }
-    return maxValue;
+  }
+  for (let j = 0; j < arr.length; j++) {
+    cur += arr[j];
+    maxValue = Math.max(cur, maxValue);
+  }
+  return maxValue;
 }
 
 // console.log(arrayManipulation(10, [[1, 5, 3], [4, 8, 7], [6, 9, 1]]));  //10
@@ -150,16 +155,16 @@ function arrayManipulation(n, queries) {
  *
  */
 function Node(data) {
-    this.data = data;
-    this.next = null;
+  this.data = data;
+  this.next = null;
 }
 
 function printLinkedList(head) {
-    var start = head;
-    while (start) {
-        console.log(start.data);
-        start = start.next;
-    }
+  let start = head;
+  while (start) {
+    console.log(start.data);
+    start = start.next;
+  }
 }
 
 // Complete the insertNodeAtHead function below.
@@ -175,22 +180,22 @@ function printLinkedList(head) {
  */
 
 const SinglyLinkedListNode = class {
-    constructor(nodeData) {
-        this.data = nodeData;
-        this.next = null;
-    }
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
 };
 
 function insertNodeAtHead(head, data) {
-    //if head is null create a node and point with head
-    var node = new SinglyLinkedListNode(data);
-    if (head == null) {
-        head = node;
-    } else {
-        node.next = head;
-        head = node;
-    }
-    return head;
+  // if head is null create a node and point with head
+  const node = new SinglyLinkedListNode(data);
+  if (head == null) {
+    head = node;
+  } else {
+    node.next = head;
+    head = node;
+  }
+  return head;
 }
 
 // let lk = insertNodeAtHead(null, 383);
@@ -222,34 +227,83 @@ function insertNodeAtHead(head, data) {
  */
 
 function insertNodeAtPosition(llist, data, position) {
-    // Write your code here
-    var node = new SinglyLinkedListNode(data);
-    let cnt = 0;
-    if (llist == null) {
-        llist = node;
-        return llist;
-    }
-    if (position === 0) {
-        node.next = llist;
-        llist = node;
-        return llist
-    }
-    var start = llist;
-    while (start.next != null && cnt < position - 1) {
-        start = start.next;
-        cnt++;
-    }
-    let temp = start.next;
-    start.next = node;
-    node.next = temp;
-
+  // Write your code here
+  const node = new SinglyLinkedListNode(data);
+  let cnt = 0;
+  if (llist == null) {
+    llist = node;
     return llist;
+  }
+  if (position === 0) {
+    node.next = llist;
+    llist = node;
+    return llist;
+  }
+  let start = llist;
+  while (start.next != null && cnt < position - 1) {
+    start = start.next;
+    cnt++;
+  }
+  const temp = start.next;
+  start.next = node;
+  node.next = temp;
+
+  return llist;
 }
 
-let lk = insertNodeAtPosition(null, 383, 2);
-lk = insertNodeAtPosition(lk, 484, 0);
-lk = insertNodeAtPosition(lk, 392, 1);
-lk = insertNodeAtPosition(lk, 975, 1);
-lk = insertNodeAtPosition(lk, 321, 2);
+// let lk = insertNodeAtPosition(null, 383, 2);
+// lk = insertNodeAtPosition(lk, 484, 0);
+// lk = insertNodeAtPosition(lk, 392, 1);
+// lk = insertNodeAtPosition(lk, 975, 1);
+// lk = insertNodeAtPosition(lk, 321, 2);
 
-printLinkedList(lk);    //484,975,
+// printLinkedList(lk); // 484,975,
+
+/*
+ * Complete the 'deleteNode' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function deleteNode(llist, position) {
+  // Write your code here
+  let cnt = 0;
+  if (llist == null) {
+    return llist;
+  }
+  let start = llist;
+  if (position === 0) {
+    llist = llist.next;
+    start.next = null;
+    return llist;
+  }
+
+  while (start.next != null && cnt < position - 1) {
+    start = start.next;
+    cnt++;
+  }
+  if (start.next !== null) {
+    const temp = start.next;
+    start.next = start.next.next;
+    temp.next = null;
+  }
+
+  return llist;
+}
+
+// lk = deleteNode(lk, 0);
+// console.log('After deletion');
+// printLinkedList(lk); // 484,975,
